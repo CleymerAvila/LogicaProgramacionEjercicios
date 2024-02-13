e3f8a8895e594161c7c2f059388f76958ba437aa
cleymer <avilacleymer05@gmail.com>
2024-02-13 13:50:42 -0500

    Segundo ejercicio

---
+++ Ejercicio2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        
+        String textoIngresado;
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingrese un texto");
+        textoIngresado = leer.nextLine();
+
+        System.out.println("El texto ingresado es: " + textoIngresado);
+
+    }
+}
