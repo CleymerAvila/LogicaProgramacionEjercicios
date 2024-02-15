9b3368d72260d4e6d532614661e606641f5f4072
cleymer <avilacleymer05@gmail.com>
2024-02-14 20:02:29 -0500

    septimo ejercicio

---
+++ Ejercicio7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int numerodado;
+        System.out.print("Ingrese un numero: ");
+        numerodado = leer.nextInt();
+
+        if (numerodado > 0) {
+            System.out.println("El numero ingresado " + numerodado + " es un numero positivo");
+        } else if (numerodado < 0) {
+            System.out.println("El numero ingresado " + numerodado + " es un numero negativo");
+        } else {
+            System.out.println("El numero ingresado no es positivo ni negativo");
+        }
+    }
+}
