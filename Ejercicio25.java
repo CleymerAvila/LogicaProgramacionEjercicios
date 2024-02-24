164ceb1f20e86db9ee26d45ada26c5b36bf954b3
cleymer <avilacleymer05@gmail.com>
2024-02-24 14:48:33 -0500

    Ejercicio 25 hecho

---
+++ Ejercicio25.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double gradosCentigrados;
+        double gradosFahrenheit;
+
+        System.out.println("Ingresa los grados en Centigrados");
+        gradosCentigrados = sc.nextDouble();
+
+        gradosFahrenheit = ((gradosCentigrados *9)/5) + 32;
+
+        System.out.println(gradosCentigrados +"°C grados centigrados equivale a "+ gradosFahrenheit + "°F Fahrenheit");
+
+    }
+}
