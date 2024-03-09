b822f3caf3d474f7407e2d175bfd1e00b0bd221e
cleymer <avilacleymer05@gmail.com>
2024-03-09 13:25:09 -0500

    Ejercicio 79 terminado

---
+++ Ejercicio79.java
@@ -0,0 +1,9 @@
+public class Ejercicio79 {
+    public static void main(String[] args) {
+        double volBombillosKv, numBombillos;
+
+        numBombillos = 3;
+        volBombillosKv = (numBombillos * 120)/1000;
+        System.out.println("El voltaje  de 3 bombillos en kilovoltios es de "+ volBombillosKv+ " kilovoltios de voltaje");
+    }
+}
