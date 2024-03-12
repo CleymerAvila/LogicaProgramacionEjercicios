9fc9e58de3fcf6e1bc580fa07cda690011574222
cleymer <avilacleymer05@gmail.com>
2024-03-11 19:27:02 -0500

    Ejercicio 84 terminado

---
+++ Ejercicio84.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ejercicio84 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int edad ;
+
+        System.out.println("Ingresa la edad");
+        edad = sc.nextInt();
+        String age = String.valueOf(edad);
+
+        System.out.println("La edad ingresa en el teclado es de "+ age+ " años.");
+        
+    }
+}
