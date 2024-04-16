dcf1b2de83cc62eef1e827f557856443c05486af
cleymer <avilacleymer05@gmail.com>
2024-04-15 20:47:23 -0500

    ejercicio 188 terminado

--- Ejercicio188.java
+++ Ejercicio188.java
@@ -4,7 +4,7 @@ public class Ejercicio188 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese una direccion de correo electronico");
-        String email = sc.next();
+        String email = sc.next(); sc.close();
 
         int arroba=0;
         int punto=0;
