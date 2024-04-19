813b8363036c90a59d2be94175a840f5916062c0
cleymer <avilacleymer05@gmail.com>
2024-04-18 22:17:02 -0500

    Ejercicio 205 listo

--- Ejercicio205.java
+++ Ejercicio205.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Ejercicio205 {
+import java.util.Scanner;
+
+public class Ejercicio205 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final double CONVERSION_BOLIVIANO_A_DOLAR = 0.14;
+
+        System.out.println("Ingrese los bolivianos a convertir a dolares");
+        double bolivianos = sc.nextDouble();
+
+        double dolares = bolivianos * CONVERSION_BOLIVIANO_A_DOLAR;
+
+        System.out.printf("\n%s %.2f", "Los bolivianos ingresados son equivalentes en dolares a ",dolares);
+    }
 }
