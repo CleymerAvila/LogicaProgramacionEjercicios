441002dd3d33858590812eaa921fcdb0bf2683c7
cleymer <avilacleymer05@gmail.com>
2024-04-20 14:56:52 -0500

    Ejericio terminado

--- Ejercicio225.java
+++ Ejercicio225.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Ejercicio225 {
+import java.util.Scanner;
+
+public class Ejercicio225 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa el lado: ");
+        int lado = Math.abs(sc.nextInt());
+
+        for (int i = 0; i < lado; i++) {
+            System.out.print("*");
+        }
+
+        for (int j = 0; j < (lado/2); j++) {
+            System.out.print("\n*");
+            for (int i = 0; i < lado-2; i++) {
+                System.out.print(" ");
+
+            }
+            System.out.print("*");
+        }
+
+        System.out.println();
+        for (int i = 0; i < lado; i++) {
+            System.out.print("*");
+
+        }
+    }
 }
